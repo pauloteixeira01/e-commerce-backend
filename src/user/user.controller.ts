7c6323dd06e526b1aed52edcb3623b3e02914d95
Paulo Teixeira <paulo.teixeira541@gmail.com>
2023-08-14 12:15:52 -0300

    feat: create user (POST)

---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import {CreateUserDto} from './dtos/createUser.dto'
+
+@Controller('user')
+export class UserController {
+    @Post()
+    async createUser(
+        @Body() createUserDto: CreateUserDto
+    ) {
+        return {
+            ...createUserDto, 
+            password: undefined
+        }
+    }
+}
